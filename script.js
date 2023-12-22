5c86b02fa3567212776d9b73e67e0d4927aa486a
mapsehh <154602691+mapsehh@users.noreply.github.com>
2023-12-22 15:29:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    document.getElementById('message').innerText = 'Данные записаны';
+  });
+  function button(url){
+    window.location.href=url;
+}
